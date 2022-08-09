3ea8b1293372b662dc82508c2431a397dbc040fb
MemoryHunter <59766692+memory-hunter@users.noreply.github.com>
2022-08-09 22:30:59 +0400

    Correction to wait before closing

--- main.cpp
+++ main.cpp
@@ -8,6 +8,12 @@ void wait() {
 }
 
 int main(int argc, char *argv[]) {
+    if (argc != 2) {
+        std::cerr << "Usage: DoJaject.exe <command>" << std::endl;
+        wait();
+        return -1;
+    }
+
     if (!boost::filesystem::exists("config.json")) {
         std::cerr << "config.json not found. Creating..." << std::endl;
         boost::property_tree::ptree root;
@@ -31,33 +37,29 @@ int main(int argc, char *argv[]) {
         boost::property_tree::write_json("config.json", root);
     }
 
-    if (argc != 2) {
-        std::cerr << "Usage: DoJaject.exe <command>" << std::endl;
+    if (std::string(argv[1]).find(".jam") || std::string(argv[1]).find(".jar")) {
+        std::cerr << "File isn't of .jam or .jar extension. Exiting..." << std::endl;
+        wait();
         return -1;
     } else {
-        if (std::string(argv[1]).find(".jam") || std::string(argv[1]).find(".jar")) {
-            std::cerr << "File isn't of .jam or .jar extension. Exiting..." << std::endl;
+        boost::property_tree::ptree root;
+        boost::property_tree::read_json("config.json", root);
+        std::string doja_path = root.get<std::string>("doja_path");
+        std::cout << "Creating a project with jam/jar combo..." << std::endl;
+        boost::filesystem::path path(doja_path + "/apps");
+        std::string name = std::string(argv[1]);
+        name.erase(std::remove(name.begin(), name.end(), '.'), name.end());
+        if (!boost::filesystem::create_directory(path / name / "bin")) {
+            std::cerr << "Could not create directory. Exiting..." << std::endl;
             wait();
             return -1;
-        } else {
-            boost::property_tree::ptree root;
-            boost::property_tree::read_json("config.json", root);
-            std::string doja_path = root.get<std::string>("doja_path");
-            std::cout << "Creating a project with jam/jar combo..." << std::endl;
-            boost::filesystem::path path(doja_path + "/apps");
-            std::string name = std::string(argv[1]);
-            name.erase(std::remove(name.begin(), name.end(), '.'), name.end());
-            if (!boost::filesystem::create_directory(path / name / "bin")){
-                std::cerr << "Could not create directory. Exiting..." << std::endl;
-                wait();
-                return -1;
-            }
-            boost::filesystem::path project_path(path / name / "bin");
-            copy_file(argv[1], project_path / argv[1]);
-            copy_file(name + ".jam", project_path / (name + ".jam"));
-            copy_file(name + ".jar", project_path / (name + ".jar"));
-            std::cout << "Project created." << std::endl;
         }
+        boost::filesystem::path project_path(path / name / "bin");
+        copy_file(argv[1], project_path / argv[1]);
+        copy_file(name + ".jam", project_path / (name + ".jam"));
+        copy_file(name + ".jar", project_path / (name + ".jar"));
+        std::cout << "Project created." << std::endl;
     }
+
     wait();
 }
