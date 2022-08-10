b907a0dfd1dd588d43f5391a5f83ca24df699ac6
MemoryHunter <59766692+memory-hunter@users.noreply.github.com>
2022-08-10 10:42:23 +0400

    Changed the way input is taken

--- main.cpp
+++ main.cpp
@@ -12,18 +12,7 @@ void wait() {
     std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
 }
 
-int main(int argc, char *argv[]) {
-    if (argc != 2) {
-        std::cerr << "Usage: DoJaject.exe <command>" << std::endl;
-        wait();
-        return -1;
-    }
-
-    /* 3 lines below are temporary, suspected a bug in Boost library when an argument is passed, the current directory changes to that of the passed argument. */
-    std::string exec_path = std::string(argv[0]).substr(0, std::string(argv[0]).find_last_of('\\'));
-    boost::filesystem::current_path(exec_path);
-    std::cout << boost::filesystem::current_path().string() << std::endl;
-
+int main() {
     if (!boost::filesystem::exists("config.json")) {
         std::cerr << "config.json not found. Creating..." << std::endl;
         boost::property_tree::ptree root;
@@ -31,7 +20,6 @@ int main(int argc, char *argv[]) {
         std::string path;
         std::getline(std::cin, path);
         boost::filesystem::path doja_path(path);
-        std::cout << doja_path.string() << std::endl;
         if (!boost::filesystem::is_directory(doja_path)) {
             std::cerr << "Not a directory. Exiting..." << std::endl;
             wait();
@@ -47,9 +35,12 @@ int main(int argc, char *argv[]) {
         boost::property_tree::write_json("config.json", root);
     }
 
-    boost::filesystem::path arg_path((std::string(argv[1])));
-
-    if (arg_path.string().find(".jar") == std::string::npos && arg_path.string().find(".jam") == std::string::npos) {
+    std::cout << "Enter game .jam or .jar from the same directory (or drag and drop): " << std::endl;
+    boost::filesystem::path arg_path;
+    std::string arg;
+    std::getline(std::cin, arg);
+    arg_path = boost::filesystem::path(arg);
+    if (arg_path.extension() != ".jam" && arg_path.extension() != ".jar") {
         std::cerr << "File isn't of .jam or .jar extension. Exiting..." << std::endl;
         wait();
         return -1;
@@ -59,11 +50,10 @@ int main(int argc, char *argv[]) {
         std::string doja_path = root.get<std::string>("doja_path");
         std::cout << "Creating a project with jam/jar combo..." << std::endl;
         game game{
-                arg_path.string().substr(arg_path.string().find_last_of('\\') + 1,
-                                         arg_path.string().find_last_of('.') - arg_path.string().find_last_of('\\') -
-                                         1),
-                arg_path.string().substr(0, arg_path.string().find_last_of('\\'))
+                arg_path.stem(),
+                arg_path.parent_path()
         };
+        std::cout << game.name << " " << game.directory << std::endl;
         boost::filesystem::path path(doja_path + "/apps/");
         boost::filesystem::current_path(path);
         boost::filesystem::create_directory(path / game.name);
@@ -77,6 +67,5 @@ int main(int argc, char *argv[]) {
                   boost::filesystem::copy_option::overwrite_if_exists);
         std::cout << "Project created." << std::endl;
     }
-
     wait();
 }
